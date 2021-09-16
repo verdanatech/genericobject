72c7fb2ee0141d17931a019d5c26caac435c6296
Dionesio Guerra <dionesiojr@gmail.com>
2021-09-16 20:15:07 -0300

    create

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => '2.11.0',
+    'version' => '2.11.0.0',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'e728d204a2a251dca387ae0a2ec83b775c098a41',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => '2.11.0',
+      'version' => '2.11.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'e728d204a2a251dca387ae0a2ec83b775c098a41',
+    ),
+  ),
+);
